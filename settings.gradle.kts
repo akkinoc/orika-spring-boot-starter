8fc0e08d09c77fb7bd51e5d5b24c94e9b3f50d0a
Akihiro Kondo <akkinoc@gmail.com>
2026-05-16 01:09:08 +0900

    Add gradle plugin: org.gradle.toolchains.foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,15 @@
-rootProject.name = "orika-spring-boot-starter"
-
-include("orika-spring-boot-starter")
-include("examples:simple-java")
-include("examples:simple-kotlin")
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
 }
+
+rootProject.name = "orika-spring-boot-starter"
+
+include("orika-spring-boot-starter")
+include("examples:simple-java")
+include("examples:simple-kotlin")
